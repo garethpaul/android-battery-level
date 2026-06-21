ffbed53ecc390404d680a1f2eea98b2a5de88c6e
Gareth Jones <gareth@garethpaul.com>
2026-06-20 19:38:27 -0700

    test: distinguish mutation failures from toolchain errors

--- scripts/test-battery-host.sh
+++ scripts/test-battery-host.sh
@@ -7,13 +7,30 @@ trap 'rm -rf "$BUILD_DIR"' EXIT HUP INT TERM
 
 mkdir -p "$BUILD_DIR/classes"
 
-javac -source 7 -target 7 -d "$BUILD_DIR/classes" \
-  "$ROOT_DIR/host-tests/src/android/os/Build.java" \
-  "$ROOT_DIR/host-tests/src/android/util/Log.java" \
-  "$ROOT_DIR/app/src/main/java/garethpaul/com/chargeme/BatteryTelemetry.java" \
-  "$ROOT_DIR/app/src/main/java/garethpaul/com/chargeme/OneLineReader.java" \
-  "$ROOT_DIR/app/src/main/java/garethpaul/com/chargeme/SMemTextReader.java" \
-  "$ROOT_DIR/app/src/main/java/garethpaul/com/chargeme/CurrentReader.java" \
-  "$ROOT_DIR/host-tests/src/garethpaul/com/chargeme/BatteryHostTest.java"
+if javac -source 7 -target 7 -d "$BUILD_DIR/classes" \
+    "$ROOT_DIR/host-tests/src/android/os/Build.java" \
+    "$ROOT_DIR/host-tests/src/android/util/Log.java" \
+    "$ROOT_DIR/app/src/main/java/garethpaul/com/chargeme/BatteryTelemetry.java" \
+    "$ROOT_DIR/app/src/main/java/garethpaul/com/chargeme/OneLineReader.java" \
+    "$ROOT_DIR/app/src/main/java/garethpaul/com/chargeme/SMemTextReader.java" \
+    "$ROOT_DIR/app/src/main/java/garethpaul/com/chargeme/CurrentReader.java" \
+    "$ROOT_DIR/host-tests/src/garethpaul/com/chargeme/BatteryHostTest.java"; then
+  :
+else
+  exit $?
+fi
 
-java -cp "$BUILD_DIR/classes" garethpaul.com.chargeme.BatteryHostTest
+JAVA_LOG="$BUILD_DIR/java.log"
+if java -cp "$BUILD_DIR/classes" garethpaul.com.chargeme.BatteryHostTest >"$JAVA_LOG" 2>&1; then
+  cat "$JAVA_LOG"
+  exit 0
+else
+  java_status=$?
+fi
+
+cat "$JAVA_LOG" >&2
+if grep -Fq "java.lang.AssertionError" "$JAVA_LOG" && \
+   grep -Fq "garethpaul.com.chargeme.BatteryHostTest" "$JAVA_LOG"; then
+  exit 42
+fi
+exit "$java_status"
